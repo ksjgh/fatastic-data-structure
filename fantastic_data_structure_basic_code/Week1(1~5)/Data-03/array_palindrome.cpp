08b26cc17b48d3507ac03cc1e44a57e50fda3905
Sung Jin <ksjksjgg@gmail.com>
2018-07-15 18:37:30 +0900

    w1 3강 1차원 배열의 활용

--- fantastic_data_structure_basic_code/Week1(1~5)/Data-03/array_palindrome.cpp
+++ fantastic_data_structure_basic_code/Week1(1~5)/Data-03/array_palindrome.cpp
@@ -1,4 +1,73 @@
+//////template
+//#define MY_SOL
+//#define REF_SOL
+//
+//#ifdef MY_SOL
+//
+//#endif // MY_SOL
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+//#ifdef REF_SOL
+//
+//#endif // REF_SOL
 
+
+#define MY_SOL
+//#define REF_SOL
+
+#ifdef MY_SOL
+#include <stdio.h>
+#include <string.h>
+
+void inputString(char *cp);
+int checkPalindrome(char *cp);
+
+int main()
+{
+	char str[80];		// 문자열 저장 배열
+	int res;			// 회문 검사 결과 저장
+
+	inputString(str);
+	while(strcmp(str,"end")!=0){
+		res = checkPalindrome(str);
+		if(res==1)
+			printf("\"%s\" : 회문입니다.\n", str);
+		else
+			printf("\"%s\" : 회문이 아닙니다.\n", str);
+		inputString(str);
+	}
+
+	getchar();
+	getchar();
+	return 0;
+}
+//---------------------------------------------------
+void inputString(char *cp)
+{
+	printf("# 회문 검사할 문자열을 입력하시오 : ");
+	scanf("%s", cp);
+	return;
+}
+//---------------------------------------------------
+int checkPalindrome(char *cp)
+{
+	// TODO
+	unsigned int len = strlen(cp);
+	int result = 1;
+	for(int i = 0 ; i < len ; i++){
+        if(cp[i] != cp[len-1-i]){
+            result = 0;
+            break;
+        }
+	}
+	return result;
+}
+#endif // MY_SOL
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+#ifdef REF_SOL
 #include <stdio.h>
 #include <string.h>
 
@@ -37,3 +106,6 @@ int checkPalindrome(char *cp)
 	// TODO
 	return 0;
 }
+#endif // REF_SOL
+
+
--- fantastic_data_structure_basic_code/Week1(1~5)/Data-03/array_search.cpp
+++ fantastic_data_structure_basic_code/Week1(1~5)/Data-03/array_search.cpp
@@ -1,3 +1,7 @@
+//#define MY_SOL
+#define REF_SOL
+
+#ifdef MY_SOL
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -34,17 +38,112 @@ int main()
 int searchArray(int *ap, unsigned int size, int sData)
 {
 	// TODO
+	int i;
+	bool fFound=false;
+	for(i=0; i<size; i++){
+        if(ap[i]==sData){
+            fFound = true;
+            break;
+        }
+	}
+
+    if(fFound==true){
+        return i;
+    }
+    else
+        return -1;
 
 	return 0;
 }
+
 //---------------------------------------------------
 void initArray(int *ap, unsigned int size)
 {
 	// TODO
+	for(int i=0; i < size ; i++){
+        ap[i] = rand()%20+1;
+	}
 }
 //---------------------------------------------------
 void printArray(int *ap, unsigned int cnt)
 {
 	// TODO
+	int i;
+	printf("배열의 내용 : ");
+	for (i = 0; i < cnt; i++) {
+		printf("%5d", *ap++);
+	}
+	printf("\n");
 }
+#endif // MY_SOL
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+#ifdef REF_SOL
+#include <stdio.h>
+#include <stdlib.h>
+void printArray(int *ap, unsigned int cnt); // 배열내용 출력 함수
+void initArray(int *ap, unsigned int size); // 1~20까지의 난수로 배열초기화하는 함수
+int searchArray(int *ap, unsigned int size, int sData); // 검색함수
+
+int main()
+{
+	int num[10];		// 데이터 저장 배열
+	unsigned int size;  // 배열의 크기(방이 개수) 저장
+	int sData;			// 검색할 데이터 저장
+	int res=0;			// 검색 결과 저장
+	int i;
+	size = sizeof(num)/sizeof(num[0]);	// 배열 크기 계산
+
+	initArray(num, size);
+	printArray(num, size);
+
+	for(i=0; i<5; i++){
+		printf("# 검색할 정수를 입력하시오 : ");
+		scanf("%d", &sData);
+		res = searchArray(num, size, sData);
+		if(res!=-1)
+			printf("%d는 %d번 방에 있습니다.\n", sData, res);
+		else
+			printf("%d는 존재하지 않습니다.\n", sData);
+	}
+	getchar();
+	getchar();
+	return 0;
+}
+//---------------------------------------------------
+int searchArray(int *ap, unsigned int size, int sData)
+{
+	// TODO
+	int pos = -1;
+	unsigned int i;
+	for(i=0; i<size ; i++){
+        if(ap[i]==sData){
+            pos = i;
+            break;
+        }
+	}
+
+	return pos;
+}
+//---------------------------------------------------
+void initArray(int *ap, unsigned int size)
+{
+	// TODO
+	for(int i=0; i < size ; i++){
+        ap[i] = rand()%20+1;
+	}
+}
+//---------------------------------------------------
+void printArray(int *ap, unsigned int cnt)
+{
+	// TODO
+	int i;
+	printf("배열의 내용 : ");
+	for (i = 0; i < cnt; i++) {
+		printf("%5d", *ap++);
+	}
+	printf("\n");
+}
+#endif // REF_SOL
 
